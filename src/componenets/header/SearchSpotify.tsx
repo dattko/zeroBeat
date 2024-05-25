d84048d61bbbbfed974aeec6caf22dafa66d95e5
Min's Git <99301568+dattko@users.noreply.github.com>
2024-05-25 19:16:05 +0900

    서치 스타일 추가 및 포커스 시 안내 옵션 추가

--- src/componenets/header/SearchSpotify.tsx
+++ src/componenets/header/SearchSpotify.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 interface Artist {
   name: string;
@@ -15,6 +16,7 @@ export const SearchComponent = () => {
   const [searchResults, setSearchResults] = useState<Track[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
 
   const handleSearch = async () => {
     if (!searchTerm.trim()) return;
@@ -37,27 +39,73 @@ export const SearchComponent = () => {
     setIsLoading(false);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div className='input-box'>
       <input
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search tracks"
+        placeholder="검색어를 입력해 주세요."
+        onKeyDown={handleKeyDown}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
       <button className='icon-btn n-b' style={{ width: '34px' }}
         onClick={handleSearch} disabled={isLoading}>
         <img src="/images/search.svg" alt="로고" />
       </button>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      <ul>
-        {searchResults.map((track, index) => (
-          <li key={index}>
-            {track.name} by {track.artists.map((artist: Artist) => artist.name).join(", ")}
-          </li>
-        ))}
-      </ul>
+      {isFocused && (
+        <InputContent>
+          {isLoading && <p>Loading...</p>}
+          {error && <span>로그인 후 이용해 주세요.</span>}
+          <SeachUl>
+            {searchResults.map((track, index) => (
+              <SeachLi key={index}>
+                {track.name} by {track.artists.map((artist: Artist) => artist.name).join(", ")}
+              </SeachLi>
+            ))}
+          </SeachUl>
+        </InputContent>
+      )}
     </div>
   );
 };
+
+const InputContent = styled.div`
+  z-index: 10;
+  position: absolute;
+  border-radius: 4px;
+  width: calc(100% - 20px);
+  padding: 15px 10px;
+  background-color: rgba(255, 255, 255, 0.8);
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  top: calc(100% + 10px);
+  span{
+    font-size: 13px;
+  }
+`;
+
+const SeachUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  font-size: 14px;
+  max-height: 600px;
+  overflow: auto  ;
+`
+const SeachLi = styled.li`
+cursor: pointer;
+padding: 10px 12px;
+border-radius: 6px;
+&:hover{
+  background-color: #f1f1f1;
+}
+`
